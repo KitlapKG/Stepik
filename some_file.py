770af4a2ddc4a70a209f90e5e22056310fd61fe9
Bektur Malikov <nadrim.018@gmail.com>
2024-02-25 16:58:15 +0300

    has been added another_some_function

--- some_file.py
+++ some_file.py
@@ -2,3 +2,8 @@ print('This is a file from GitHub repository')
 
 
 print('These are new local changes')
+
+def another_some_function(number: int | float | complex = 0) -> None:
+    return number * 5
+
+print(another_some_function(3))
