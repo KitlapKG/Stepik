d7c4478b0239e9465efa1bc1ae75a741ce8970ba
Kitlap <nadrim.018@gmail.com>
2024-02-28 14:05:41 +0300

    start new file about cats

---
+++ cats.py
@@ -0,0 +1,26 @@
+import requests
+import time
+
+
+API_URL = 'https://api.telegram.org/bot'
+BOT_TOKEN = '6629668359:AAFO4NXiPUZtRK8Moffj294sMyy8dm3zWa0'
+offset = -2
+timeout = 60
+updates: dict
+
+
+def do_something() -> None:
+    print('Был апдейт')
+
+
+while True: 
+    start_time = time.time()
+    updates = requests.get(f'{API_URL}{BOT_TOKEN}/getUpdates?offset={offset + 1}&timeout={timeout}').json()
+
+    if updates['result']:
+        for result in updates['result']:
+            offset = result['update_id']
+            do_something()
+
+    end_time = time.time()
+    print(f'Время между запросами к Telegram Bot API: {end_time - start_time}')
